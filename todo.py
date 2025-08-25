3540f769ba36873db4a86296bf07a797ac8603c8
市原諒 <usermail@mail.com>
2025-08-25 11:33:42 +0900

    挨拶メッセージを追加 (feature/hello)

--- todo.py
+++ todo.py
@@ -2,15 +2,21 @@
 tasks = []
 
 def add_task():
-    tasks.append(task)
+    tasks.append({"task":task, "done":done})
+
+def delete_task():
+    if 0 <= index < len(tasks):
+        tasks.pop(index)
 
 def show_tasks():
     for i, task in enumerate(tasks, 1):
-        print(f"{i}. {task}")
+        status = "✓" if task["done"] else "✗"
+        print(f"{i}. {task['task']} [{status}]")
+    print("=== hello from feature branch! ===")
 
 if __name__ == "main":
     add_task("Git学習を始める")
-    add_task("ToDoアプリを作る")
+    add_task("ToDoアプリを作る", done=True)
     show_tasks
     delete_task(0)  # 最初のタスクを削除
     show_tasks()
